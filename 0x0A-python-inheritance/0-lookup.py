e341ef8dd728b06d25a476adabcc02440659a643
Blueskale <princewillmmadu@gmail.com>
2023-04-11 06:05:41 -0700

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+=============================
+Module with the method lookpu
+=============================
+"""
+
+
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Function for return the attributes for an object"""
+
     return dir(obj)
 
